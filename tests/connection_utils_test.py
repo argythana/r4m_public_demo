ae17d4b7b5bba7b68b10ad0353e80af169b34c51
ΘΑΝΑΣΗΣ ΑΡΓΥΡΙΟΥ <argythana@gmail.com>
2024-06-01 18:25:26 +0300

    Remove test ssh.

--- tests/connection_utils_test.py
+++ tests/connection_utils_test.py
@@ -2,18 +2,15 @@
 Functions to test the connections to the remote servers and to mongo Databases.
 """
 
-import typing
-from typing import Any
+import os
 import sys
+import typing
 from datetime import datetime
 from unittest.mock import MagicMock, patch
-import unittest
+
 import pytest
 from pymongo.collection import Collection
 from pymongo.database import Database
-
-
-import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.insert(0, "./../src")
@@ -35,28 +32,6 @@ def db() -> MagicMock:
 @typing.no_type_check
 def db_collection() -> MagicMock:
     return MagicMock(spec=Collection)
-
-
-class TestSSHConnection(unittest.TestCase):
-    @patch("utils.connection_utils.SSHTunnelForwarder")
-    def test_ssh_to_remote_server(self, mock_ssh_tunnel: Any) -> None:
-        # Create a mock for SSHTunnelForwarder
-        mock_ssh_tunnel_instance = MagicMock()
-        mock_ssh_tunnel.return_value = mock_ssh_tunnel_instance
-
-        # Call your function with the arguments
-        server_info = {"ip": "192.168.0.1", "port": 22, "username": "tester"}
-        ssh_connection_instance = connection_utils.ssh_to_remote_server(server_info)
-
-        # Verify that SSHTunnelForwarder was called with the correct arguments
-        mock_ssh_tunnel.assert_called_once_with(
-            ssh_address_or_host=(server_info["ip"], server_info["port"]),
-            ssh_username=server_info["username"],
-            remote_bind_address=("localhost", 27017),
-        )
-
-        # Verify that the result is the mock SSHTunnelForwarder
-        self.assertEqual(ssh_connection_instance, mock_ssh_tunnel_instance)
 
 
 def test_create_datetime_init_check(
