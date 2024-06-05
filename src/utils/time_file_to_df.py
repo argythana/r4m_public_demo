61acf9c6d5dfe5acea8ea40044a6636577affcde
ΘΑΝΑΣΗΣ ΑΡΓΥΡΙΟΥ <argythana@gmail.com>
2024-06-05 12:30:55 +0300

    Add read filetypes times and tests.

--- src/utils/time_file_to_df.py
+++ src/utils/time_file_to_df.py
@@ -1,11 +1,12 @@
 """
-Compare the time it takes to read data using different file formats and libraries.
+Compare the time to read data using different file formats and libraries.
 """
 
 import os
 import timeit
 
 import pandas as pd
+import polars as pl
 
 # Get the absolute path of the directory of the current file
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -15,25 +16,56 @@ csv_file_path = os.path.join(dir_path, "..", "data", "obfuscated_data.csv")
 feather_file_path = os.path.join(dir_path, "..", "data", "obfuscated_data.feather")
 
 
-def read_csv_file() -> pd.DataFrame:
+def pandas_read_csv_file() -> pd.DataFrame:
     df: pd.DataFrame = pd.read_csv(csv_file_path)
     return df
 
 
-def read_feather_file() -> pd.DataFrame:
+def pandas_read_csv_file_use_pyarrow() -> pd.DataFrame:
+    df: pd.DataFrame = pd.read_csv(csv_file_path, engine="pyarrow")
+    return df
+
+
+def pandas_read_feather_file() -> pd.DataFrame:
     df: pd.DataFrame = pd.read_feather(feather_file_path)
     return df
 
 
-# Time using pandas from csv file
-time_taken_csv = timeit.timeit(read_csv_file, number=1000)
+def polars_read_csv_file() -> pl.DataFrame:
+    df: pl.DataFrame = pl.read_csv(csv_file_path)
+    return df
 
 
-# Time using pandas from feather file
-time_taken_feather = timeit.timeit(read_feather_file, number=1000)
+def polars_read_feather_file() -> pl.DataFrame:
+    df: pl.DataFrame = pl.read_ipc(feather_file_path, memory_map=False)
+    return df
 
+
+# Number of times to run the function to get the average time
+NUMBER = 500
+
+# Time pandas read csv
+pandas_read_csv_time = timeit.timeit(pandas_read_csv_file, number=NUMBER)
+
+# Time pandas read csv using pyarrow
+pandas_read_csv_use_pyarrow_time = timeit.timeit(
+    pandas_read_csv_file_use_pyarrow, number=NUMBER
+)
+
+# Time pandas read csv feather
+pandas_read_feather_time = timeit.timeit(pandas_read_feather_file, number=NUMBER)
+
+
+# Time polars read csv
+polars_read_csv_time = timeit.timeit(polars_read_csv_file, number=NUMBER)
+
+# Time polars read feather
+polars_read_feather_time = timeit.timeit(polars_read_feather_file, number=NUMBER)
 
 print(
-    f"Time for pandas to read the csv file 1000 times: {time_taken_csv} seconds.\n"
-    f"Time for pandas to read the feather file 1000 times: {time_taken_feather} seconds.",
+    f"Time for pandas to read the csv file {NUMBER} times: {pandas_read_csv_time} seconds.\n"
+    f"Time for pandas to read the csv file using pyarrow {NUMBER} times: {pandas_read_csv_use_pyarrow_time} seconds.\n"
+    f"Time for pandas to read the feather file {NUMBER} times: {pandas_read_feather_time} seconds.\n"
+    f"Time for polars to read the csv file {NUMBER} times: {polars_read_csv_time} seconds.\n"
+    f"Time for polars to read the feather version 2 file {NUMBER} times: {polars_read_feather_time} seconds.\n"
 )
--- tests/time_file_to_df_test.py
+++ tests/time_file_to_df_test.py
@@ -2,24 +2,47 @@ import sys
 import unittest
 
 import pandas as pd
+import polars as pl
 
-# src to path
-
-sys.path.append("../src")
+# run from tests directory
 sys.path.append("../src/utils")
 
-from time_file_to_df import read_csv_file, read_feather_file
+# run from r4m_public directory
+sys.path.append("./src/utils")
+
+from time_file_to_df import (
+    pandas_read_csv_file,
+    pandas_read_csv_file_use_pyarrow,
+    pandas_read_feather_file,
+    polars_read_csv_file,
+    polars_read_feather_file,
+)
 
 
 class TestTimeFileToDf(unittest.TestCase):
-    def test_read_csv_file(self) -> None:
-        df = read_csv_file()
+    def test_pandas_read_csv_file(self) -> None:
+        df = pandas_read_csv_file()
         self.assertIsInstance(df, pd.DataFrame)
         return None
 
-    def test_read_feather_file(self) -> None:
-        df = read_feather_file()
+    def test_pandas_read_feather_file(self) -> None:
+        df = pandas_read_feather_file()
         self.assertIsInstance(df, pd.DataFrame)
+        return None
+
+    def test_pandas_read_csv_file_use_pyarrow(self) -> None:
+        df = pandas_read_csv_file_use_pyarrow()
+        self.assertIsInstance(df, pd.DataFrame)
+        return None
+
+    def test_polars_read_csv_file(self) -> None:
+        df = polars_read_csv_file()
+        self.assertIsInstance(df, pl.DataFrame)
+        return None
+
+    def test_polars_read_feather_file(self) -> None:
+        df = polars_read_feather_file()
+        self.assertIsInstance(df, pl.DataFrame)
         return None
 
 
