bc50bfd501fa0a7d09406b6983a19193fc0e461e
ΘΑΝΑΣΗΣ ΑΡΓΥΡΙΟΥ <argythana@gmail.com>
2024-06-05 12:29:22 +0300

    Add data files and tests.

--- src/data/csv_to_feather.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read the CSV file
-df = pd.read_csv("obfuscated_data.csv")
-
-# Save the DataFrame as a Feather v2 file
-df.to_feather("obfuscated_data_v2.feather", version=2)
