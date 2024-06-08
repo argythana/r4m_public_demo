f295109f833469fa7d55063c7708f9c4881b28f9
ΘΑΝΑΣΗΣ ΑΡΓΥΡΙΟΥ <argythana@gmail.com>
2024-06-08 13:38:18 +0300

    Define daily plots function.

---
+++ src/utils/demo_streamlit_colors.py
@@ -0,0 +1,25 @@
+"""
+Use these methods to get the colors of the demo site.
+https://stackoverflow.com/questions/25444297/extract-a-list-of-colors-from-a-webpage
+https://alwane.io/
+"""
+
+# Dictionary for demo streamlit app and charts.
+color_template = {
+    "lines": "#0693e3",  # 00b6e6",  # Light Blue
+    "titles": "#f57e20",  # Orange
+    "background": "#152630",  # Dark Blue
+    "ticks": "#0693e3",  # Light Grey #949494
+    "demo": "#162780",
+    "dialectica": "#162780",
+}
+
+# color_template = {
+#     "lines": "#7792E3",  # 00b6e6",  # Light Blue also for header
+#     "titles": "#0693e3",
+#     "background": "#164780",  # #162780 is the official demo violet blue.
+#     # Another Dark blue plots background #180657
+#     "ticks": "#7792E3",
+# }
+
+# 0693e3 # bLue
